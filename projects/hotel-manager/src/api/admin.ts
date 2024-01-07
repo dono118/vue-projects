99406bcf65ec2e21d2095d130bce12d8bfd5b7fd
dono118 <dono118@126.com>
2024-01-07 17:20:31 +0800

    对密码进行MD5加密

--- projects/hotel-manager/src/api/admin.ts
+++ projects/hotel-manager/src/api/admin.ts
@@ -1,7 +1,18 @@
 import { $post } from '../utils/request'
+import { md5 } from 'md5js'
+
+interface loginParam {
+  username: string
+  password: string
+}
 
 // 登录方法
-export const $login = async (params: object) => {
+export const $login = async (params: loginParam) => {
+  // 对密码进行MD5加密
+  params.password = md5(
+    md5(params.password, 32).split('').reverse().join(''),
+    32
+  )
   let res = await $post('api/login', params)
   console.log(res)
 }

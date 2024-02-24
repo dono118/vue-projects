60301410f3062c13b044dbd56960279c186eb840
dono118 <dono118@126.com>
2024-02-24 17:29:00 +0800

    修改持久化插件配置

--- projects/hotel-manager/src/store/user.ts
+++ projects/hotel-manager/src/store/user.ts
@@ -14,5 +14,13 @@ export const useUserStore = defineStore('user', {
     }
   },
   // 模块开启持久化
-  persist: true
+  // persist: true
+  persist: {
+    // 存储的 key， 默认是 defineStore 的第一个参数
+    key: id => `hm_${id}`,
+    // 存储位置，默认 localStorage
+    storage: sessionStorage,
+    // 指定内容
+    paths: ['user.username']
+  }
 })

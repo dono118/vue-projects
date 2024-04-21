15b8b3d3efef8aab5fd75b4a0d070286268701bf
dono118 <dono118@126.com>
2024-04-21 12:02:01 +0800

    reactive创建对象类型的响应式数据

--- projects/hotel-manager/src/views/user/Mine.vue
+++ projects/hotel-manager/src/views/user/Mine.vue
@@ -1,21 +1,54 @@
 <template>
   <div class="mine">
-    <p>姓名：{{ name }}</p>
-    <el-button @click="changeName">修改姓名</el-button>
+    <h2>汽车信息：</h2>
+    <p>品牌：{{ car.brand }}</p>
+    <p>价格：{{ car.price }}万RMB</p>
+    <el-button type="primary" @click="changePrice(1)">涨价</el-button>
+    <el-button @click="changePrice(-1)">降价</el-button>
+    <h2>汽车销量排行：</h2>
+    <ul>
+      <li v-for="r in rankList" :key="r.id">{{ r.brand }}</li>
+    </ul>
+    <el-button type="primary" @click="changeRank">更新排名</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive } from 'vue'
 
 // 数据
-let name = ref('张三')
-
+const car = reactive({ brand: '特斯拉', price: 20 })
 // 方法
-const changeName = () => {
-  let nameArr = ['李四', 'Ann', 'Leo', 'Tiffani', 'Eric', 'Lisa']
-  let i = Math.floor(Math.random() * 6)
-  name.value = nameArr[i]
+const changePrice = (num: number) => {
+  car.price += num
+}
+
+// 数据
+const rankList = reactive([
+  {
+    id: 1,
+    brand: '丰田'
+  },
+  {
+    id: 2,
+    brand: '奔驰'
+  },
+  {
+    id: 3,
+    brand: '宝马'
+  },
+  {
+    id: 4,
+    brand: '奥迪'
+  },
+  {
+    id: 5,
+    brand: '特斯拉'
+  }
+])
+// 方法
+const changeRank = () => {
+  rankList[0].brand = '小米'
 }
 </script>
 

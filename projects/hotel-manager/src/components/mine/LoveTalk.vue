56487d8108c668cece98f4209cf8e7c54b1a7e27
dono118 <dono118@126.com>
2024-07-13 18:42:51 +0800

    pinia - actions

--- projects/hotel-manager/src/components/mine/LoveTalk.vue
+++ projects/hotel-manager/src/components/mine/LoveTalk.vue
@@ -11,21 +11,12 @@
 
 <script setup lang="ts">
 import useTalkStore from '@/store/loveTalk'
-import axios from 'axios'
-import { nanoid } from 'nanoid'
 
 // 数据
 const talkStore = useTalkStore()
 // 方法
-async function getLoveTalk() {
-  // 发请求，下面这行的写法是：连续解构赋值+重命名
-  let {
-    data: { content: title }
-  } = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
-  // 把请求回来的字符串，包装成一个对象
-  let obj = { id: nanoid(), title }
-  // 放到数组中
-  talkStore.talkList.unshift(obj)
+function getLoveTalk() {
+  talkStore.getATalk()
 }
 </script>
 
--- projects/hotel-manager/src/store/loveTalk.ts
+++ projects/hotel-manager/src/store/loveTalk.ts
@@ -1,4 +1,6 @@
 import { defineStore } from 'pinia'
+import axios from 'axios'
+import { nanoid } from 'nanoid'
 
 // defineStore方法执行会返回一个函数,函数作用就是让组件可以获取到仓库数据
 let useTalkStore = defineStore('talk', {
@@ -10,6 +12,18 @@ let useTalkStore = defineStore('talk', {
         { id: 'ftrfasdf02', title: '草莓、蓝莓、蔓越莓，今天想我了没？' },
         { id: 'ftrfasdf03', title: '心里给你留了一块地，我的死心塌地' }
       ]
+    }
+  },
+  actions: {
+    async getATalk() {
+      // 发请求，下面这行的写法是：连续解构赋值+重命名
+      let {
+        data: { content: title }
+      } = await axios.get('https://api.uomg.com/api/rand.qinghua?format=json')
+      // 把请求回来的字符串，包装成一个对象
+      let obj = { id: nanoid(), title }
+      // 放到数组中
+      this.talkList.unshift(obj)
     }
   }
 })

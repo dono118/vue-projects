cbd14a25278342e512f3924d80b24bb346545bd9
dono118 <dono118@126.com>
2024-08-10 18:12:53 +0800

    按需引入组件样式

--- projects/read-app/src/router/index.js
+++ projects/read-app/src/router/index.js
@@ -4,7 +4,7 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: () => import('')
+    component: () => import('@/App.vue')
   }
 ]
 
--- projects/read-app/vite.config.js
+++ projects/read-app/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from '@vant/auto-import-resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +12,13 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [VantResolver()]
+    }),
+    Components({
+      resolvers: [VantResolver()]
+    })
+  ]
 })

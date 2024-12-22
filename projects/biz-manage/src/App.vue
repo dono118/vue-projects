fb147540479ce8bb00b1c8ea9eb01cade0223982
dono118 <dono118@126.com>
2024-12-22 17:32:34 +0800

    biz-manage init

---
+++ projects/biz-manage/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-config-provider :size="size" :z-index="zIndex" :locale="locales">
+    <RouterView />
+  </el-config-provider>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { ElConfigProvider } from 'element-plus'
+import { RouterView } from 'vue-router'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import plusZhCn from 'plus-pro-components/es/locale/lang/zh-cn'
+// import en from 'element-plus/es/locale/lang/en'
+// import plusEn from 'plus-pro-components/es/locale/lang/en'
+
+const zhCnLocales = {
+  ...zhCn,
+  ...plusZhCn,
+}
+// const enLocales = {
+//   ...en,
+//   ...plusEn,
+// }
+
+// const language = ref('zh-cn')
+// const locale = computed(() => (language.value === 'zh-cn' ? zhCnLocales : enLocales))
+
+// const toggleLanguage = () => {
+//   language.value = language.value === 'zh-cn' ? 'en' : 'zh-cn'
+// }
+
+export default defineComponent({
+  components: {
+    ElConfigProvider,
+  },
+  setup() {
+    return {
+      zIndex: 3000,
+      size: 'small',
+      locales: zhCnLocales,
+    }
+  },
+})
+</script>
---
+++ projects/biz-manage/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    Hello, World!
+  </main>
+</template>
